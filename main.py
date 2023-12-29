fc481bba1d7aaffbb192527c594fbb3e47cc9971
glazgov <maniakk7171@gmail.com>
2023-12-29 06:07:34 +0100

    Create columns, image, description

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Marcin Jusiński")
+    content = """
+    Hi, i Am Marcin and here is my project showcase website
+    """
+
+    st.info(content)
