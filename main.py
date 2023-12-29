574f6b8737b2e4cae57f30069fb6e8534c495b21
glazgov <maniakk7171@gmail.com>
2023-12-29 06:52:21 +0100

    Add description, image, links

--- main.py
+++ main.py
@@ -22,15 +22,20 @@ You can find them below
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
-    st.image("images/1.png")
+        st.write("description")
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write("description")
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
